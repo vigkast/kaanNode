11fc609c6c5f14e15b4004697ea95c95e9d707e7
vigkast <vigwohlig@gmail.com>
2016-03-24 18:01:50 +0530

    Revert "dlete added"
    This reverts commit d10e030281af5baeca324803602fb1be76507ab8.

--- api/models/User.js
+++ api/models/User.js
@@ -166,35 +166,4 @@ module.exports = {
             }
         });
     },
-    delete: function(data, callback) {
-        sails.query(function(err, db) {
-            if (err) {
-                console.log(err);
-                callback({
-                    value: false
-                });
-            } else {
-                db.collection("user").remove().toArray(function(err, found) {
-                    if (err) {
-                        callback({
-                            value: false
-                        });
-                        db.close();
-                    } else if (found) {
-                        callback({
-                            value: true,
-                            comment: "Deleted"
-                        });
-                        db.close();
-                    } else {
-                        callback({
-                            value: false,
-                            comment: "No data found"
-                        });
-                        db.close();
-                    }
-                });
-            }
-        });
-    }
 };
